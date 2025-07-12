c95225c415fe774b233e853fd90180ac05434511
gudetamago <sharakael@yahoo.com>
2025-07-12 10:20:58 +0800

    Added shared method and removed redundant variable

--- weather.py
+++ weather.py
@@ -49,12 +49,10 @@ def calculate_mean(weather_data):
     Returns:
         A float representing the mean value.
     """
-    total_count = 0
+    total_count = len(weather_data)
     total_value = 0.0
     if bool(weather_data):
-        for weather in weather_data:
-            total_value = total_value + float(weather)
-            total_count += 1
+        total_value = sum( float(weather) for weather in weather_data)
         average = total_value / total_count
         return average
     else:
@@ -81,7 +79,25 @@ def load_data_from_csv(csv_file):
                 data.append(row)
     return data
 
+def find_extreme_in_list(weather_data, max=True):
+    """Finds the extreme value in a list of numbers.
 
+    Args:
+        weather_data: A list of numbers.
+        max: A boolean indicating whether to find the maximum (True) or minimum (False).
+    Returns:
+        The extreme value and its position in the list. (In case of multiple matches, return the index of the *last* example in the list.)
+    """
+    extreme_value = float(weather_data[0])
+    extreme_index = 0
+        
+    for index, item in enumerate(weather_data):
+        if (max and float(item) >= extreme_value) or (not max and float(item) <= extreme_value):
+            extreme_value = round(float(item), 1)
+            extreme_index = index
+            
+    return (extreme_value, extreme_index)
+    
 def find_min(weather_data):
     """Calculates the minimum value in a list of numbers.
 
@@ -92,15 +108,7 @@ def find_min(weather_data):
     """
     
     if bool(weather_data):
-        minimum_value = float(weather_data[0])
-        minimum_index = 0
-        
-        for index, item in enumerate(weather_data):
-            if float(item) <= float(minimum_value):
-                minimum_value = round(float(item), 1)
-                minimum_index = index
-            
-        return (minimum_value, minimum_index)
+        return find_extreme_in_list(weather_data, max=False)
     else:
         minimum_value = ()
         return minimum_value
@@ -117,14 +125,7 @@ def find_max(weather_data):
     """
     
     if bool(weather_data):
-        maximum_value = float(weather_data[0])
-        maximum_index = 0
-    
-        for index, item in enumerate(weather_data):
-            if float(item) >= float(maximum_value):
-                maximum_value = round(float(item), 1)
-                maximum_index = index
-        return (maximum_value, maximum_index)
+        return find_extreme_in_list(weather_data, max=True)
     else:
         maximum_value = ()
         return maximum_value
@@ -153,19 +154,16 @@ def generate_summary(weather_data):
     
     
     lowest_temp = find_min(list_of_mins)
-    
     display_min_temp = format_temperature(lowest_temp[0])
-    
     date_lowest_temp = convert_date(weather_data[lowest_temp[1]][0])
+    
     highest_temp = find_max(list_of_max)
     display_max_temp = format_temperature(highest_temp[0])
     date_highest_temp = convert_date(weather_data[highest_temp[1]][0])
+    
     average_low = format_temperature(round(total_mins / total_days, 1))
     average_high = format_temperature(round(total_max / total_days, 1))
     
-    day_lowest_temp = convert_date( weather_data[lowest_temp[1]][0] )
-    day_highest_temp = convert_date( weather_data[highest_temp[1]][0] )
-
     summary = f"{total_days} Day Overview\n"
     summary += f"  The lowest temperature will be {display_min_temp}, and will occur on {date_lowest_temp}.\n"
     summary += f"  The highest temperature will be {display_max_temp}, and will occur on {date_highest_temp}.\n"
